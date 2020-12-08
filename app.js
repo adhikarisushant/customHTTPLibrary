ba6baded71067c4905beca15bde0609362e2f625
Sushant <sushant.adhikari.619@gmail.com>
2020-12-08 21:16:35 +0545

    post, put, delete request

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+// Get single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, posts) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+// create data
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+}
+
+// Create POST 
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//     if(err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
     if(err) {
         console.log(err);
-    } else {
-        console.log(posts);
+    }
+    else {
+        console.log(response);
     }
 });
+
 
